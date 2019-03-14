d50a731a908d70278110c329d569684c8ffbf7a1
German Casares <german.casares@outlook.com>
2019-03-14 01:19:59 -0700

    persistence: Create UnitOfWork

---
+++ 03.Persistence/Persistence/Data/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using Persistence.Data.Repositories.Interfaces;
+using System;
+
+namespace Persistence.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AlmanimeContext _context;
+
+        public UnitOfWork(
+            AlmanimeContext context,
+            IAnimeRepository animes
+            )
+        {
+            _context = context;
+            Animes = animes;
+        }
+
+        public IAnimeRepository Animes { get; }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Save() => _context.SaveChanges();
+    }
+}
