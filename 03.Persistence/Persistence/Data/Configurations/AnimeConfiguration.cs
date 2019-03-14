ac82b86b8a9f59dadc5d672cc2b00d5b2f86df05
German Casares <german.casares@outlook.com>
2019-03-14 01:18:52 -0700

    persistence: Create context with AnimeConfiguration

---
+++ 03.Persistence/Persistence/Data/Configurations/AnimeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Data.Configurations
+{
+    public class AnimeConfiguration : IEntityTypeConfiguration<Anime>
+    {
+        public void Configure(EntityTypeBuilder<Anime> builder)
+        {
+            builder.HasIndex(i => i.KitsuID).IsUnique();
+        }
+    }
+}
