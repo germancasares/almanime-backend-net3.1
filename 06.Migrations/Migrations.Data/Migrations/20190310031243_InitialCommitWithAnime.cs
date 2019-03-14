689a57072ea0eec06cec6c27c36725316bf32056
German Casares <german.casares@outlook.com>
2019-03-14 01:20:27 -0700

    migrations: Add Anime migration

---
+++ 06.Migrations/Migrations.Data/Migrations/20190310031243_InitialCommitWithAnime.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace Migrations.Data.Migrations
+{
+    public partial class InitialCommitWithAnime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animes",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    ModificationDate = table.Column<DateTime>(nullable: false),
+                    KitsuID = table.Column<int>(nullable: false),
+                    Slug = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Season = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    Synopsis = table.Column<string>(nullable: true),
+                    Episodes = table.Column<int>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: true),
+                    EndDate = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animes", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animes_KitsuID",
+                table: "Animes",
+                column: "KitsuID",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animes");
+        }
+    }
+}
