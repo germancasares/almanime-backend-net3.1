815b35b4feb28b9ff02abff7d4d679002609bbcc
German Casares <german.casares@outlook.com>
2019-03-14 01:24:19 -0700

    functions: Add AnimeUpdater

---
+++ 07.Functions/Functions/Models/Links.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Functions.Models
+{
+    public class Links
+    {
+        [JsonProperty("first")]
+        public string First { get; set; }
+        [JsonProperty("next")]
+        public string Next { get; set; }
+        [JsonProperty("last")]
+        public string Last { get; set; }
+    }
+}
---
+++ 07.Functions/Functions/Models/Meta.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Functions.Models
+{
+    public class Meta
+    {
+        [JsonProperty("count")]
+        public string Count { get; set; }
+    }
+}
