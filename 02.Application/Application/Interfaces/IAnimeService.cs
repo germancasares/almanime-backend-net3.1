9f8e5a325246ce07e2d0063e5957ce67651be08a
German Casares <german.casares@outlook.com>
2019-03-14 01:20:56 -0700

    application: Create AnimeService

---
+++ 02.Application/Application/Interfaces/IAnimeService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.DTOs;
+using Domain.Models;
+
+namespace Application.Services.Interfaces
+{
+    public interface IAnimeService
+    {
+        Anime GetByKitsuID(int kitsuID);
+        Anime GetBySlug(string slug);
+
+        Anime Create(AnimeDTO animeDTO);
+        void Update(AnimeDTO animeDTO);
+    }
+}
