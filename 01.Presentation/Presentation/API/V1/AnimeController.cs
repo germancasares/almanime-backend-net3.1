375d306390740118a7d118261b0c90687fb0726a
German Casares <german.casares@outlook.com>
2019-03-16 16:31:43 -0700

    presentation: Move controllers to API/V1

---
+++ 01.Presentation/Presentation/API/V1/AnimeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class AnimeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
