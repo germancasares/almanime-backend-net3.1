fccda215eb9340fa4b7961aa2a83ac01fec4cf23
German Casares <german.casares@outlook.com>
2019-03-18 20:00:07 -0700

    presentation: Add AnimeController initial implementation
    Contains GetByID, GetBySlug and GetSeason endpoints.

--- 01.Presentation/Presentation/API/V1/AnimeController.cs
+++ 01.Presentation/Presentation/API/V1/AnimeController.cs
@@ -1,12 +1,79 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Application.Services.Interfaces;
+using AutoMapper;
+using Domain.Constants;
+using Domain.Models;
+using Domain.VMs;
+using Domain.Enums;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Infrastructure.Helpers;
 
 namespace Presentation.Controllers
 {
-    public class AnimeController : Controller
+    [Route("api/v1/[controller]")]
+    public class AnimeController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ILogger<AnimeController> _logger;
+        private readonly IMapper _mapper;
+        private readonly IAnimeService _animeService;
+
+        public AnimeController(
+            ILogger<AnimeController> logger,
+            IAnimeService animeService,
+            IMapper mapper
+        )
         {
-            return View();
+            _logger = logger;
+            _animeService = animeService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{ID}")]
+        public IActionResult Get(Guid ID)
+        {
+            _logger.LogInformation(LoggingEvents.GetItem, "Request recived for path /api/anime/{ID}", ID);
+
+            var anime = _animeService.GetByID(ID);
+
+            if (anime == null) return NotFound();
+ 
+            return Ok(_mapper.Map<Anime, AnimeVM>(anime));
+        }
+
+        [HttpGet("Slug={Slug}")]
+        public IActionResult GetBySlug(string slug)
+        {
+            _logger.LogInformation(LoggingEvents.GetItem, "Request recived for path /api/animes/Slug={slug}", slug);
+
+            var anime = _animeService.GetBySlug(slug);
+
+            if (anime == null) return NotFound();
+
+            return Ok(_mapper.Map<Anime, AnimeVM>(anime));
+        }
+
+        [HttpGet("Year={year}&Season={season}")]
+        public IActionResult GetSeason(
+            int year,
+
+            string season)
+        {
+            _logger.LogInformation(LoggingEvents.GetItem, "Request recived for path /api/animes?Year={year}&Season={season}", year, season);
+
+            var seasonEnum = EnumHelper.GetEnumFromString<Season>(season);
+
+            if (!seasonEnum.HasValue) return BadRequest("Season not valid.");
+
+            var requestedSeason = _animeService
+                .GetSeason(year, seasonEnum.Value)
+                .OrderByDescending(a => string.IsNullOrWhiteSpace(a.CoverImageUrl))
+                .ThenBy(a => a.Name)
+                .ToList();
+
+            return Ok(_mapper.Map<List<Anime>, List<AnimeVM>>(requestedSeason));
         }
     }
 }
