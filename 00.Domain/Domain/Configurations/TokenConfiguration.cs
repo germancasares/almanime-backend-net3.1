a046d7d9b1ec2c18d908a838c8af4c3b04688ade
German Casares <german.casares@outlook.com>
2019-04-20 14:13:49 -0700

    domain: Add TokenConfiguration mapped from Environment Variables

---
+++ 00.Domain/Domain/Configurations/TokenConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Configurations
+{
+    public class TokenConfiguration
+    {
+        public string Secret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int AccessExpirationDays { get; set; }
+        public int RefreshExpirationDays { get; set; }
+    }
+}
