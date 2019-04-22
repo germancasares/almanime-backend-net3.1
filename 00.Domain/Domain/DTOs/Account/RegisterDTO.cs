49b2e6d3d6009fcc67870222f6e229d613235f2a
German Casares <german.casares@outlook.com>
2019-04-21 23:28:50 -0700

    domain: Change UserName to Username

--- 00.Domain/Domain/DTOs/Account/RegisterDTO.cs
+++ 00.Domain/Domain/DTOs/Account/RegisterDTO.cs
@@ -3,7 +3,7 @@
     public class RegisterDTO
     {
         public string Email { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
     }
 }
