6f9754d0da097e8027f71f7a07bfef9accf1c255
German Casares <german.casares@outlook.com>
2019-06-23 17:57:45 -0700

    migrations: Add migrations for Fansub changes

---
+++ 06.Migrations/Migrations.Data/Migrations/20190624005035_Fansub.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Migrations.Data.Migrations
+{
+    public partial class Fansub : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fansubs",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    ModificationDate = table.Column<DateTime>(nullable: false),
+                    Acronym = table.Column<string>(nullable: true),
+                    FullName = table.Column<string>(nullable: true),
+                    LogoUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fansubs", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Membership",
+                columns: table => new
+                {
+                    FansubID = table.Column<Guid>(nullable: false),
+                    UserID = table.Column<Guid>(nullable: false),
+                    Role = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Membership", x => new { x.FansubID, x.UserID });
+                    table.ForeignKey(
+                        name: "FK_Membership_Fansubs_FansubID",
+                        column: x => x.FansubID,
+                        principalTable: "Fansubs",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Membership_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Membership_UserID",
+                table: "Membership",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Membership");
+
+            migrationBuilder.DropTable(
+                name: "Fansubs");
+        }
+    }
+}
