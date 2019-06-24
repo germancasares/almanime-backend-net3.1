121cc3c9d141d071b9f1d2d4f42225f437f3577a
German Casares <german.casares@outlook.com>
2019-06-23 17:58:18 -0700

    presentation: Add FansubController

---
+++ 01.Presentation/Presentation/API/V1/FansubController.cs
@@ -0,0 +1,44 @@
+﻿using Application.Interfaces;
+using AutoMapper;
+using Domain.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Presentation.API.V1
+{
+    [ValidateModel]
+    [Route("api/v1/[controller]")]
+    public class FansubController : ControllerBase
+    {
+
+        private readonly ILogger<FansubController> _logger;
+        private readonly IMapper _mapper;
+        private readonly IFansubService _fansubService;
+
+        public FansubController(
+            ILogger<FansubController> logger,
+            IFansubService fansubService,
+            IMapper mapper
+        )
+        {
+            _logger = logger;
+            _fansubService = fansubService;
+            _mapper = mapper;
+        }
+
+        [HttpPost("")]
+        public IActionResult Create([FromBody] FansubDTO fansubDTO)
+        {
+            var fansub = _fansubService.Create(fansubDTO);
+
+            return Ok(fansub);
+        }
+
+        [HttpDelete("{fansubID}")]
+        public void Delete(Guid fansubID)
+        {
+            _fansubService.Delete(fansubID, new Guid("7E542F5F-BF09-453D-8D29-D81D6117FD8F"));
+        }
+    }
+}
