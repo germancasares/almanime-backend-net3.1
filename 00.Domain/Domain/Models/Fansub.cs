ceeaeb0398d37bdad88679a7ce71ccf76a673a6c
German Casares <german.casares@outlook.com>
2019-06-23 17:55:26 -0700

    domain: Add Fansub, Membership entity and EFansubRole enum

---
+++ 00.Domain/Domain/Models/Fansub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public class Fansub : BaseModel
+    {
+        public string Acronym { get; set; }
+        public string FullName { get; set; }
+        public string LogoUrl { get; set; }
+
+        public virtual List<Membership> Memberships { get; set; }
+
+        public Fansub()
+        {
+            Memberships = new List<Membership>();
+        }
+    }
+}
