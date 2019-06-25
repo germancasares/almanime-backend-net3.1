f0c96e9d248e2772485e687f36aca59b6c7a928f
German Casares <german.casares@outlook.com>
2019-06-24 23:52:25 -0700

    jobs: Add security classes to validate jwt token

---
+++ 07.Jobs/Jobs/Security/AccessTokenBindingProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+
+namespace Jobs.Security
+{
+    /// <summary>
+    /// Provides a new binding instance for the function host.
+    /// </summary>
+    public class AccessTokenBindingProvider : IBindingProvider
+    {
+        public Task<IBinding> TryCreateAsync(BindingProviderContext context)
+        {
+            IBinding binding = new AccessTokenBinding();
+            return Task.FromResult(binding);
+        }
+    }
+}
---
+++ 07.Jobs/Jobs/Security/AccessTokenExtensionProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.WebJobs.Host.Config;
+
+namespace Jobs.Security
+{
+    /// <summary>
+    /// Wires up the attribute to the custom binding.
+    /// </summary>
+    public class AccessTokenExtensionProvider : IExtensionConfigProvider
+    {
+        public void Initialize(ExtensionConfigContext context) => context.AddBindingRule<AccessTokenAttribute>().Bind(new AccessTokenBindingProvider());
+    }
+}
