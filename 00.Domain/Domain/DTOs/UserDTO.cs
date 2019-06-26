d365d1661a71760914e21aac74d033108e38dd90
German Casares <german.casares@outlook.com>
2019-06-26 00:24:05 -0700

    domain: Update UserDTO

---
+++ 00.Domain/Domain/DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Domain.DTOs
+{
+    public class UserDTO
+    {
+        public string NickName { get; set; }
+        public IFormFile Avatar { get; set; }
+    }
+}
