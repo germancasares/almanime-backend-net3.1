e8c78bbbc2d24812d04ab6b525ae418fa5101bc0
German Casares <german.casares@outlook.com>
2019-06-26 00:26:12 -0700

    presentation: Change WithErrorCode to WithMessage on Validators

--- 01.Presentation/Presentation.Validators/RegisterDTOValidator.cs
+++ 01.Presentation/Presentation.Validators/RegisterDTOValidator.cs
@@ -11,39 +11,39 @@ namespace Presentation.Validators
         {
             RuleFor(r => r.Password)
                 .NotEmpty()
-                .WithErrorCode(ValidationCode.NotEmpty.ToString());
+                .WithMessage(ValidationCode.NotEmpty.ToString());
             RuleFor(r => r.Password)
                 .Must(p => p.Any(char.IsDigit))
-                .WithErrorCode(ValidationCode.HasDigit.ToString());
+                .WithMessage(ValidationCode.HasDigit.ToString());
             RuleFor(r => r.Password)
                 .MinimumLength(6)
-                .WithErrorCode(ValidationCode.MinimumLength.ToString());
+                .WithMessage(ValidationCode.MinimumLength.ToString());
             RuleFor(r => r.Password)
                 .Must(p => p.Any(char.IsLower))
-                .WithErrorCode(ValidationCode.HasLowerCase.ToString());
+                .WithMessage(ValidationCode.HasLowerCase.ToString());
             RuleFor(r => r.Password)
                 .Must(p => !p.All(char.IsLetterOrDigit))
-                .WithErrorCode(ValidationCode.HasNonAlphanumeric.ToString());
+                .WithMessage(ValidationCode.HasNonAlphanumeric.ToString());
             RuleFor(r => r.Password)
                 .Must(p => p.Any(char.IsUpper))
-                .WithErrorCode(ValidationCode.HasUpperCase.ToString());
+                .WithMessage(ValidationCode.HasUpperCase.ToString());
 
             RuleFor(r => r.Email)
                 .NotEmpty()
-                .WithErrorCode(ValidationCode.NotEmpty.ToString());
+                .WithMessage(ValidationCode.NotEmpty.ToString());
             RuleFor(r => r.Email)
                 .EmailAddress()
-                .WithErrorCode(ValidationCode.ValidEmailAddress.ToString());
+                .WithMessage(ValidationCode.ValidEmailAddress.ToString());
             RuleFor(r => r.Email)
                 .MustAsync(async (email, _) => !await accountService.ExistsEmail(email))
-                .WithErrorCode(ValidationCode.Unique.ToString());
+                .WithMessage(ValidationCode.Unique.ToString());
 
             RuleFor(r => r.Username)
                 .NotEmpty()
-                .WithErrorCode(ValidationCode.NotEmpty.ToString());
+                .WithMessage(ValidationCode.NotEmpty.ToString());
             RuleFor(r => r.Username)
                 .MustAsync(async (username, _) => !await accountService.ExistsUsername(username))
-                .WithErrorCode(ValidationCode.Unique.ToString());
+                .WithMessage(ValidationCode.Unique.ToString());
         }
     }
 }
