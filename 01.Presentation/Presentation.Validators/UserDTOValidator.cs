6ba133ffa748daf42efd7308d6a5d186a87733f5
German Casares <german.casares@outlook.com>
2019-06-26 00:25:40 -0700

    presentation: Add Validator for UserDTO

---
+++ 01.Presentation/Presentation.Validators/UserDTOValidator.cs
@@ -0,0 +1,51 @@
+﻿using Application.Interfaces;
+using Domain.DTOs;
+using FluentValidation;
+using Infrastructure.Helpers;
+using SixLabors.ImageSharp;
+
+namespace Presentation.Validators
+{
+    public class UserDTOValidator : AbstractValidator<UserDTO>
+    {
+        public UserDTOValidator(IUserService userService)
+        {
+            When(r => r.NickName != null, () =>
+            {
+                RuleFor(r => r.NickName)
+                    .Must(nickName => !userService.ExistsNickName(nickName))
+                    .WithMessage(ValidationCode.Unique.ToString());
+            });
+
+            When(r => r.Avatar != null, () =>
+            {
+                RuleFor(r => r.Avatar.ContentType)
+                    .Must(contentType => IsImage(contentType))
+                    .WithMessage(ValidationCode.ContentTypeNotValid.ToString());
+                RuleFor(r => r.Avatar.Length)
+                    .LessThanOrEqualTo(2.MbToBytes())
+                    .WithMessage(ValidationCode.MaximumLength.ToString());
+
+                When(r => IsImage(r.Avatar.ContentType), () =>
+                {
+                    RuleFor(r => r.Avatar)
+                        .Must(avatar =>
+                        {
+                            var image = Image.Load(avatar.OpenReadStream());
+                            return image.Width == image.Height;
+                        })
+                        .WithMessage(ValidationCode.ImageAspectRatio.ToString());
+                    RuleFor(r => r.Avatar)
+                        .Must(avatar =>
+                        {
+                            var image = Image.Load(avatar.OpenReadStream());
+                            return image.Width <= 512 && image.Height <= 512;
+                        })
+                        .WithMessage(ValidationCode.ImageResolution.ToString());
+                });
+            });
+        }
+
+        private bool IsImage(string contentType) => contentType == "image/png" || contentType == "image/jpg" || contentType == "image/jpeg";
+    }
+}
--- 01.Presentation/Presentation.Validators/ValidationCode.cs
+++ 01.Presentation/Presentation.Validators/ValidationCode.cs
@@ -4,14 +4,18 @@
     {
         NotEmpty = 1,
         Unique = 2,
+        MinimumLength = 3,
+        MaximumLength = 4,
+        ContentTypeNotValid = 5,
+        ImageAspectRatio = 6,
+        ImageResolution = 7,
 
         // Account
         ValidEmailAddress = 1001,
         IdentifierExists = 1002,
-        MinimumLength = 1003,
-        HasDigit = 1004,
-        HasLowerCase = 1005,
-        HasNonAlphanumeric = 1006,
-        HasUpperCase = 1007
+        HasDigit = 1003,
+        HasLowerCase = 1004,
+        HasNonAlphanumeric = 105,
+        HasUpperCase = 1006,
     }
 }
