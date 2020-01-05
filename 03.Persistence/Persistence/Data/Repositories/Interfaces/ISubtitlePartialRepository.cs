192a882480defc3bb76f97acbcc91fad3becbae4
German Casares <german.casares@outlook.com>
2020-01-04 23:26:24 -0800

    persistence: Update Subtitle configurations and repositories

--- 03.Persistence/Persistence/Data/Repositories/Interfaces/ISubtitlePartialRepository.cs
+++ 03.Persistence/Persistence/Data/Repositories/Interfaces/ISubtitlePartialRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Persistence.Data.Repositories.Interfaces
+﻿using Domain.Models;
+using System;
+
+namespace Persistence.Data.Repositories.Interfaces
 {
     public interface ISubtitlePartialRepository
     {
+        SubtitlePartial Create(SubtitlePartial entity);
+        void Delete(Guid id);
+        void Delete(SubtitlePartial subtitlePartial);
+        SubtitlePartial GetByID(Guid id);
     }
 }
--- 03.Persistence/Persistence/Data/Repositories/SubtitlePartialRepository.cs
+++ 03.Persistence/Persistence/Data/Repositories/SubtitlePartialRepository.cs
@@ -1,8 +1,20 @@
-﻿using Persistence.Data.Repositories.Interfaces;
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data.Repositories.Interfaces;
+using System;
+using System.Linq;
 
 namespace Persistence.Data.Repositories
 {
     public class SubtitlePartialRepository : ISubtitlePartialRepository
     {
+        private readonly DbSet<SubtitlePartial> _dbSet;
+
+        public SubtitlePartialRepository(AlmanimeContext context) => _dbSet = context.Set<SubtitlePartial>();
+
+        public SubtitlePartial GetByID(Guid id) => _dbSet.SingleOrDefault(o => o.ID == id);
+        public SubtitlePartial Create(SubtitlePartial entity) => _dbSet.Add(entity).Entity;
+        public void Delete(Guid id) => Delete(GetByID(id));
+        public void Delete(SubtitlePartial subtitlePartial) => _dbSet.Remove(subtitlePartial);
     }
 }
