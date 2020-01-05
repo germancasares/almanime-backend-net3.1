747c896107fa6a96295049c821345293a0fa70d2
German Casares <german.casares@outlook.com>
2020-01-04 23:29:42 -0800

    application: Add method to create Subtitles

--- 02.Application/Application/Interfaces/ISubtitleService.cs
+++ 02.Application/Application/Interfaces/ISubtitleService.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Domain.DTOs;
+using Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
     public interface ISubtitleService
     {
+        Task<Subtitle> Create(SubtitleDTO subtitleDTO, Guid identityID);
         Domain.Models.Subtitle GetByID(Guid ID);
     }
 }
