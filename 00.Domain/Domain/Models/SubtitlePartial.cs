ca364b032cb8496b961830a8e88a80d26d13250f
German Casares <german.casares@outlook.com>
2020-01-04 23:22:37 -0800

    domain: Modify Subtitle models and add DTOs

--- 00.Domain/Domain/Models/SubtitlePartial.cs
+++ 00.Domain/Domain/Models/SubtitlePartial.cs
@@ -4,13 +4,14 @@ namespace Domain.Models
 {
     public class SubtitlePartial
     {
+        public Guid ID { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Url { get; set; }
+
         public Guid SubtitleID { get; set; }
         public virtual Subtitle Subtitle { get; set; }
 
         public Guid UserID { get; set; }
         public virtual User User { get; set; }
-
-        public int Revision { get; set; }
-        public DateTime RevisionDate { get; set; }
     }
 }
