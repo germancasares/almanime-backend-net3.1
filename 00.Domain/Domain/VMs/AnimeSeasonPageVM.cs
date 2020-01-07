a88b9d0925ac41f17c8e6ba7c264200c6422d084
German Casares <german.casares@outlook.com>
2020-01-06 21:05:05 -0800

    domain: Add models for AnimePagination and VMs

---
+++ 00.Domain/Domain/VMs/AnimeSeasonPageVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.VMs
+{
+    public class AnimeSeasonPageVM
+    {
+        public PaginationMetaVM Meta { get; set; }
+        public List<AnimeVM> Animes { get; set; }
+    }
+}
