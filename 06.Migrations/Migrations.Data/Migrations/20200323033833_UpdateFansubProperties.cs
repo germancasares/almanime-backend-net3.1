c324cbc77027ef45f16b460d925a7b341f757373
German Casares <german.casares@outlook.com>
2020-03-25 20:52:56 -0700

    migrations: Add migration for the new properties of the fansub

---
+++ 06.Migrations/Migrations.Data/Migrations/20200323033833_UpdateFansubProperties.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Migrations.Data.Migrations
+{
+    public partial class UpdateFansubProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MainLanguage",
+                table: "Fansubs",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MembershipOption",
+                table: "Fansubs",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Webpage",
+                table: "Fansubs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MainLanguage",
+                table: "Fansubs");
+
+            migrationBuilder.DropColumn(
+                name: "MembershipOption",
+                table: "Fansubs");
+
+            migrationBuilder.DropColumn(
+                name: "Webpage",
+                table: "Fansubs");
+        }
+    }
+}
