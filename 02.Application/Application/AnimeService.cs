020cbc77dc0f621e59ad02abf95152f1370032df
German Casares <german.casares@outlook.com>
2020-06-02 23:05:21 -0700

    application: Move methods from AnimeService to EpisodeService

--- 02.Application/Application/AnimeService.cs
+++ 02.Application/Application/AnimeService.cs
@@ -6,7 +6,6 @@ using Domain.Models;
 using Persistence.Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Application.Services
 {
@@ -15,7 +14,10 @@ namespace Application.Services
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
-        public AnimeService(IUnitOfWork unitOfWork, IMapper mapper)
+        public AnimeService(
+            IUnitOfWork unitOfWork, 
+            IMapper mapper
+        )
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
@@ -24,11 +26,6 @@ namespace Application.Services
         public Anime GetByID(Guid guid) => _unitOfWork.Animes.GetByID(guid);
         public Anime GetByKitsuID(int kitsuID) => _unitOfWork.Animes.GetByKitsuID(kitsuID);
         public Anime GetBySlug(string slug) => _unitOfWork.Animes.GetBySlug(slug);
-        public IEnumerable<Episode> GetEpisodes(Guid guid) => _unitOfWork.Animes.GetByID(guid).Episodes;
-        public IEnumerable<Episode> GetEpisodesBySlug(string slug) => _unitOfWork.Animes.GetBySlug(slug).Episodes;
-        public Episode GetEpisode(Guid animeID, int number) => _unitOfWork.Animes.GetByID(animeID).Episodes.SingleOrDefault(c => c.Number == number);
-        public Episode GetEpisodeBySlug(string slug, int number) => _unitOfWork.Animes.GetBySlug(slug).Episodes.SingleOrDefault(c => c.Number == number);
-
         public int GetAnimesInSeason(int year, ESeason season) => _unitOfWork.Animes.GetAnimesInSeason(year, season);
         public IEnumerable<Anime> GetSeason(int year, ESeason season) => _unitOfWork.Animes.GetSeason(year, season);
 
---
+++ 02.Application/Application/EpisodeService.cs
@@ -0,0 +1,49 @@
+﻿using Application.Interfaces;
+using AutoMapper;
+using Domain.DTOs;
+using Domain.Models;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+
+namespace Application
+{
+    public class EpisodeService : IEpisodeService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public Episode GetByID(Guid guid) => _unitOfWork.Episodes.GetByID(guid);
+        public Episode GetByAnimeIDAndNumber(Guid guid, int number) => _unitOfWork.Episodes.GetByAnimeIDAndNumber(guid, number);
+        public Episode GetByAnimeSlugAndNumber(string animeSlug, int number) => _unitOfWork.Episodes.GetByAnimeSlugAndNumber(animeSlug, number);
+        public IEnumerable<Episode> GetByAnimeID(Guid guid) => _unitOfWork.Episodes.GetByAnimeID(guid);
+        public IEnumerable<Episode> GetByAnimeSlug(string animeSlug) => _unitOfWork.Episodes.GetByAnimeSlug(animeSlug);
+
+        public EpisodeService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public Episode Create(EpisodeDTO episodeDTO)
+        {
+            var episode = _unitOfWork.Episodes.Create(_mapper.Map<EpisodeDTO, Episode>(episodeDTO));
+
+            episode.Anime = _unitOfWork.Animes.GetBySlug(episodeDTO.AnimeSlug);
+
+            _unitOfWork.Save();
+
+            return episode;
+        }
+
+        public void Update(EpisodeDTO episodeDTO)
+        {
+            var episode = GetByAnimeSlugAndNumber(episodeDTO.AnimeSlug, episodeDTO.Number);
+
+            if (episode == null) return;
+
+            _unitOfWork.Episodes.Update(_mapper.Map(episodeDTO, episode));
+            _unitOfWork.Save();
+        }
+    }
+}
