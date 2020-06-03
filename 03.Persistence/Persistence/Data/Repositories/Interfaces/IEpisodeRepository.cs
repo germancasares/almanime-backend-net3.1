fb3968b053d84253bf6444ad433b6c7338712437
German Casares <german.casares@outlook.com>
2020-06-03 15:42:05 -0700

    persistence: Add methods to EpisodeRepository

--- 03.Persistence/Persistence/Data/Repositories/Interfaces/IEpisodeRepository.cs
+++ 03.Persistence/Persistence/Data/Repositories/Interfaces/IEpisodeRepository.cs
@@ -1,11 +1,18 @@
 ﻿using Domain.Models;
+<<<<<<< HEAD
+=======
+using System;
+>>>>>>> persistence: Add methods to EpisodeRepository
 using System.Linq;
 
 namespace Persistence.Data.Repositories.Interfaces
 {
     public interface IEpisodeRepository : IBaseRepository<Episode>
     {
-        Episode GetByNumber(string animeSlug, int number);
+        Episode GetByAnimeIDAndNumber(Guid guid, int number);
+        Episode GetByAnimeSlugAndNumber(string animeSlug, int number);
         IQueryable<Episode> GetCompletedByFansub(string acronym);
+        IQueryable<Episode> GetByAnimeID(Guid guid);
+        IQueryable<Episode> GetByAnimeSlug(string animeSlug);
     }
 }
