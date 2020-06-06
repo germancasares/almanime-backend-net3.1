f10e85f1a218318cfb8012091d598e6d047fb8d5
German Casares <german.casares@outlook.com>
2020-06-06 12:45:14 -0700

    domain: Add ExceptionMessage constant class

---
+++ 00.Domain/Domain/Constants/ExceptionMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Constants
+{
+    public static class ExceptionMessage
+    {
+        public const string ESeasonOutOfRange = "Month out of ESeason range.";
+        public const string ESubtitleFormatOutOfRange = "Extension out of ESubtitleFormat range.";
+    }
+}
