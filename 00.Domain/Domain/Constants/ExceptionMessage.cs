c3939433daa9461f9ea493203299850fb32a0b03
German Casares <german.casares@outlook.com>
2020-06-08 00:22:07 -0700

    domain: Add new Exceptionj messages

--- 00.Domain/Domain/Constants/ExceptionMessage.cs
+++ 00.Domain/Domain/Constants/ExceptionMessage.cs
@@ -4,5 +4,7 @@
     {
         public const string ESeasonOutOfRange = "Month out of ESeason range.";
         public const string ESubtitleFormatOutOfRange = "Extension out of ESubtitleFormat range.";
+        public const string OnlyFounderCanDeleteFansub = "Only the founder of the fansub can delete it.";
+        public const string UserDoesntBelongOnFansub = "User does not belong on the fansub.";
     }
 }
