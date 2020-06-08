8161ce271c7156ca3df9ecbeedf619cf54573320
German Casares <german.casares@outlook.com>
2020-06-07 21:32:46 -0700

    infrastructure: Add LogLevelMap dictionary

--- 04.Infrastructure/Infrastructure/Helpers/Log.cs
+++ 04.Infrastructure/Infrastructure/Helpers/Log.cs
@@ -8,7 +8,14 @@ namespace Infrastructure.Helpers
     {
         public static Dictionary<ELoggingEvent, LogLevel> LevelMap { get; } = new Dictionary<ELoggingEvent, LogLevel>
         {
-            { ELoggingEvent.GetItem, LogLevel.Information },
+            { ELoggingEvent.AnimeCreated, LogLevel.Information },
+            { ELoggingEvent.AnimeUpdated, LogLevel.Information },
+            { ELoggingEvent.EpisodeCreated, LogLevel.Information },
+            { ELoggingEvent.EpisodeUpdated, LogLevel.Information },
+
+            { ELoggingEvent.AnimeStatusNotInRange, LogLevel.Warning },
+            { ELoggingEvent.SlugIsDelete, LogLevel.Warning },
+            { ELoggingEvent.StartDateNotRecognized, LogLevel.Warning },
         };
     }
 }
