441bb489a6d27f728496f0e94226d51e41133be4
German Casares <german.casares@outlook.com>
2020-06-07 21:22:50 -0700

    domain: Update constant class to Enum

---
+++ 00.Domain/Domain/Enums/ELoggingEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Enums
+{
+    public enum ELoggingEvent
+    {
+        GetItem = 1000,
+
+        // Jobs
+        SlugIsDelete = 9001,
+        EAnimeStatusNotInRange = 9002,
+        StartDateNotRecognized = 9003,
+        AnimeUpdated = 9004,
+        AnimeCreated = 9005,
+        EpisodeCreated = 9006,
+        EpisodeUpdated = 9007,
+    }
+}
