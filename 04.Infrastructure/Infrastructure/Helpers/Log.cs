a7f934a49ff712a9ed18c740aea80d20d29bac2b
German Casares <german.casares@outlook.com>
2020-06-08 00:22:31 -0700

    infrastructure: Add new mappings for the new logging events

--- 04.Infrastructure/Infrastructure/Helpers/Log.cs
+++ 04.Infrastructure/Infrastructure/Helpers/Log.cs
@@ -8,14 +8,33 @@ namespace Infrastructure.Helpers
     {
         public static Dictionary<ELoggingEvent, LogLevel> LevelMap { get; } = new Dictionary<ELoggingEvent, LogLevel>
         {
+            // Services
+            { ELoggingEvent.CantCreateAccount, LogLevel.Error },
+            { ELoggingEvent.CantUploadSubtitle, LogLevel.Error },
+            { ELoggingEvent.CantUploadSubtitlePartial, LogLevel.Error },
+            { ELoggingEvent.CantLinkSubtitleUrl, LogLevel.Error },
+            { ELoggingEvent.CantUploadAvatar, LogLevel.Error },
+
+            { ELoggingEvent.UserIsNotFounder, LogLevel.Warning },
+            { ELoggingEvent.UserDoesntBelongOnFansub, LogLevel.Warning },
+
+            { ELoggingEvent.AnimeSlugDoesntExist, LogLevel.Information },
+            { ELoggingEvent.UserDoesntExist, LogLevel.Information },
+            { ELoggingEvent.BookmarkAlreadyExists, LogLevel.Information },
+            { ELoggingEvent.BookmarkDoesntExist, LogLevel.Information },
+            { ELoggingEvent.FansubDoesNotExist, LogLevel.Information },
+            { ELoggingEvent.EpisodeDoesntExist, LogLevel.Information },
+            { ELoggingEvent.UserAlreadyExists, LogLevel.Information },
+
+            // Jobs
+            { ELoggingEvent.AnimeStatusNotInRange, LogLevel.Warning },
+            { ELoggingEvent.SlugIsDelete, LogLevel.Warning },
+            { ELoggingEvent.StartDateNotRecognized, LogLevel.Warning },
+
             { ELoggingEvent.AnimeCreated, LogLevel.Information },
             { ELoggingEvent.AnimeUpdated, LogLevel.Information },
             { ELoggingEvent.EpisodeCreated, LogLevel.Information },
             { ELoggingEvent.EpisodeUpdated, LogLevel.Information },
-
-            { ELoggingEvent.AnimeStatusNotInRange, LogLevel.Warning },
-            { ELoggingEvent.SlugIsDelete, LogLevel.Warning },
-            { ELoggingEvent.StartDateNotRecognized, LogLevel.Warning },
         };
     }
 }
