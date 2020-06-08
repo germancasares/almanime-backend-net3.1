7068cc2030ba84f340448804420a52dee52d65cb
German Casares <german.casares@outlook.com>
2020-06-08 00:21:52 -0700

    domain: Add new events for the Services

--- 00.Domain/Domain/Enums/ELoggingEvent.cs
+++ 00.Domain/Domain/Enums/ELoggingEvent.cs
@@ -4,6 +4,22 @@
     {
         GetItem = 1000,
 
+        // Services
+        CantCreateAccount = 2001,
+        AnimeSlugDoesntExist = 2002,
+        UserDoesntExist = 2003,
+        BookmarkAlreadyExists = 2004,
+        BookmarkDoesntExist = 2005,
+        UserIsNotFounder = 2006,
+        FansubDoesNotExist = 2007,
+        EpisodeDoesntExist = 2008,
+        UserDoesntBelongOnFansub = 2009,
+        CantUploadSubtitle = 2010,
+        CantUploadSubtitlePartial = 2011,
+        CantLinkSubtitleUrl = 2012,
+        UserAlreadyExists = 2013,
+        CantUploadAvatar = 2014,
+
         // Jobs
         SlugIsDelete = 7001,
         AnimeStatusNotInRange = 7002,
