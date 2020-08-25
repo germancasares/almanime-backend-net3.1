dd36d4f2984faf8376be1b9111276cb5ab03df32
German Casares <german.casares@outlook.com>
2020-08-24 21:21:09 -0700

    jobs: Add new Anime models

---
+++ 07.Jobs/Jobs/Models/Anime/AnimeDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Jobs.Models.Anime
+{
+    public class AnimeDataModel
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public AnimeAttributesModel Attributes { get; set; }
+    }
+}
