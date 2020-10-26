20ec34433d44191502559ae06669488d50a18e8a
German Casares <german.casares@outlook.com>
2020-10-25 17:49:03 -0700

    persistence: Add Fluent Configurations for Anime and Fansub

--- 03.Persistence/Persistence/Data/Configurations/AnimeConfiguration.cs
+++ 03.Persistence/Persistence/Data/Configurations/AnimeConfiguration.cs
@@ -10,8 +10,15 @@ namespace Persistence.Data.Configurations
             base.Configure(builder);
 
             builder
-                .HasIndex(i => i.KitsuID)
+                .HasIndex(a => a.KitsuID)
                 .IsUnique();
+
+            builder.Property(a => a.KitsuID).IsRequired();
+            builder.Property(a => a.Slug).IsRequired();
+            builder.Property(a => a.Name).IsRequired();
+            builder.Property(a => a.Season).IsRequired();
+            builder.Property(a => a.Status).IsRequired();
+            builder.Property(a => a.StartDate).IsRequired();
         }
     }
 }
